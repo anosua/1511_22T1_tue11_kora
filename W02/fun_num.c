a8c3736aaabbfde71be31681e515841c29a3b957
Anosua Roy <64733993+anosua@users.noreply.github.com>
2022-03-01 15:05:50 +1100

    Create fun_num.c

---
+++ W02/fun_num.c
@@ -0,0 +1,13 @@
+// Tutorial example of constants
+// Anosua Roy (z5264396)
+// 2022-Feb-22
+#include <stdio.h>
+#define FUNNY_NUMBER 73
+
+int main(void) {
+    int num = 73;
+    printf("%d\n", num);
+    printf("%d\n", FUNNY_NUMBER);
+    
+    return 0;
+}
