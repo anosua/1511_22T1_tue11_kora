7a78c8bf1d742fc3a317d7818fa60c0959c15e79
Anosua Roy <64733993+anosua@users.noreply.github.com>
2022-03-01 15:05:15 +1100

    Create const_style.c

---
+++ W02/const_style.c
@@ -0,0 +1,12 @@
+// Print the size of a rectangle
+
+#include <stdio.h>
+
+#define LENGTH 2
+#define WIDTH 4
+
+int main(void) {
+    printf("Our rectangle is %d by %d and its area is %d squared", LENGTH, WIDTH, LENGTH * WIDTH);
+    
+    return 0;
+}
