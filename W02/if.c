3556576be38016e9ee48240e40bf2ada5405e4b5
Anosua Roy <64733993+anosua@users.noreply.github.com>
2022-03-01 15:07:35 +1100

    Create if.c

---
+++ W02/if.c
@@ -0,0 +1,18 @@
+// If statement example
+// Anosua Roy z5264396
+// 2022-Feb-22
+
+// 
+// if (condition) {
+//     code_statements;    
+// }
+//
+
+#include <stdio.h>
+
+int main(void) {
+    // Example
+    if (!1 || 0) {
+        printf("-3 is true.\n");
+    } 
+}
