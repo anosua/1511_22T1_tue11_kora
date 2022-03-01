fcdfc1cf7632ecdbf9944b8540c8ce531f45e835
Anosua Roy <64733993+anosua@users.noreply.github.com>
2022-03-01 15:00:39 +1100

    Create hello.c

---
+++ W1/hello.c
@@ -0,0 +1,15 @@
+// Live demo of a hello world program
+// Anosua Roy z5264396
+// 2021/02/15
+
+#include <stdio.h>
+
+int main(void) {
+    
+  printf("~ ~\n0 0\n");
+  printf(" o\n");
+  printf("\\_/\n");
+  
+    
+    return 0;
+}
