075667c30499658aa3767f7554376f948b7dbf7d
Anosua Roy <64733993+anosua@users.noreply.github.com>
2022-03-29 14:24:31 +1100

    Create string.c

---
+++ W07/string.c
@@ -0,0 +1,19 @@
+// Live demo of string explanation
+// Anosua Roy z5264396
+// 2022/03/29
+
+#include <stdio.h>
+
+#define MAX_LINE_LENGTH 100
+
+int main(void) {
+
+    char my_string[MAX_LINE_LENGTH] = {'h', 'e', 'l', 'l', 'o', '\n', '!', '!', '\0'};
+    char another_string[MAX_LINE_LENGTH] = "Hey there!!!";
+    
+    char array_of_chars[MAX_LINE_LENGTH] = {'h', 'e', 'l', 'l', 'o', '\n', '!', '!'};
+    
+    
+    
+    return 0;
+}
